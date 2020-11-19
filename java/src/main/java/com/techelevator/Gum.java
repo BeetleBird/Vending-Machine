20fcf169748b9564b63731993a0e1e22a3009778
Derek Boggon <dcboggon@gmail.com>
2020-11-19 12:25:12 -0500

    Push to Alex

--- java/src/main/java/com/techelevator/Gum.java
+++ java/src/main/java/com/techelevator/Gum.java
@@ -1,5 +1,35 @@
 package com.techelevator;
 
-public class Gum {
+import materials.Sellables;
+
+public class Gum implements Sellables {
+	private String name;
+	private double price;
+	private String slotLocation;
+	private int quantity = 5;
+	
+	
+	public Gum(String name, double price, String slotLocation, int quantity) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.slotLocation = slotLocation;
+		this.quantity = quantity;
+	}
+
+
+	@Override
+	public String items() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public double price() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 
 }
